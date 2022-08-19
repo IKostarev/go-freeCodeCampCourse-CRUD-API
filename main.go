4597e4e2ce840210a8cd4161792db0078521cdd7
IKostarev <ilya.kostarev.9@gmail.com>
2022-08-19 12:56:53 +0500

    Add to move and create get method by getting movies

--- main.go
+++ main.go
@@ -1,6 +1,13 @@
 package main
 
-import "github.com/gorilla/mux"
+import (
+	"encoding/json"
+	"fmt"
+	"log"
+	"net/http"
+
+	"github.com/gorilla/mux"
+)
 
 type Movie struct {
 	ID       string    `json:"id"`
@@ -16,13 +23,35 @@ type Director struct {
 
 var movies []Movie
 
+func getMovies(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(movies)
+}
+
 func main() {
 	r := mux.NewRouter()
+
+	movies = append(movies, Movie{
+		ID:       "1",
+		Isbn:     "438227",
+		Title:    "Movie One",
+		Director: &Director{firstName: "John", lastName: "Sit"},
+	})
+
+	movies = append(movies, Movie{
+		ID:       "2",
+		Isbn:     "45455",
+		Title:    "Movie Two",
+		Director: &Director{firstName: "Steve", lastName: "Smith"},
+	})
 
 	r.HandleFunc("/movies", getMovies).Methods("GET")
 	r.HandleFunc("/movies", createMovie).Methods("POST")
 
 	r.HandleFunc("/movies/{id}", getMovie).Methods("GET")
-	r.HandleFunc("/movies{id}", updateMovie).Methods("PUT")	
-	r.HandleFunc("/movies{id}", deleteMovie).Methods("DELETE")
+	r.HandleFunc("/movies/ {id}", updateMovie).Methods("PUT")
+	r.HandleFunc("/movies/{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Starting server at port 8080\n")
+	log.Fatal(http.ListenAndServe(":8080", r))
 }
